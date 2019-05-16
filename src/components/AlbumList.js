e90339b9c1ae34bd04e42abbc3c7b882b2e3c034
Chaim Reisman <chaimoosh@gmail.com>
2019-05-16 16:49:07 -0400

    added stuffs

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -10,11 +11,17 @@ class AlbumList extends Component {
     .then(data => this.setState({ albums: data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album =>
+       <AlbumDetail key={album.title} album={album} />
+      );
+  }
+
   render() {
     console.log(this.state)
     return (
       <View>
-        <Text>Hello</Text>
+        { this.renderAlbums()}
       </View>
     );
   }
